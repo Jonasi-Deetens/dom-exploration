5248401b9d1eabfdf2afad9978358b4d3c4b143a
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-30 11:51:51 +0100

    Add functionalilty to js to generate list of names in a random order

--- 03/script.js
+++ 03/script.js
@@ -1,7 +1,15 @@
 const article = document.getElementsByTagName("article")[0];
 const learners = ["Jonasi", "Thiery", "Allesandro", "Alex", "Pieter", "Eduarda"];
+let randomLearners = [];
 
-learners.forEach(learner => {
+while (learners.length !== 0) {
+    let randomValue = Math.round(Math.random()*(learners.length-1));
+    
+    randomLearners.push(learners[randomValue]);
+    learners.splice(randomValue, 1);
+}
+
+randomLearners.forEach(learner => {
     const newSection = document.createElement("section");
     const rgb = [Math.random()*250, Math.random()*250, Math.random()*250];
     newSection.style.backgroundColor = 'rgb('+ rgb[0] +', '+ rgb[1] +', '+ rgb[2] +')';
@@ -20,4 +28,5 @@ function contrast(rgb) {
     brightness = (299*rgb[0] + 587*rgb[1] + 114*rgb[2]) / 1000
 
     return (brightness > threshold) ? '#000000' : '#ffffff';
-  }
+}
+
