a23434748237631c93ecb9c6b08cac693d86f1ff
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-30 14:38:56 +0100

    Add JS function that hides text when age is lower than 18 -06

--- 06/script.js
+++ 06/script.js
@@ -1,11 +1,23 @@
 const changeLabelText = (e) => {
     const span = document.querySelector("#display-firstname");
-
     let text = document.createTextNode(e.target.value);
 
     span.innerHTML = "";
     span.appendChild(text);
 }
 
-const input = document.querySelector("input");
+const unhideText = (e) => {
+    const truthSection = document.querySelector("#a-hard-truth");
+    if (e.target.value >= 18) {
+        truthSection.style = "";
+    } else {
+        truthSection.style.visibility = "hidden";
+    }
+}
+
+const inputs = document.querySelectorAll("input");
+const input = inputs[0];
 input.addEventListener("keyup", changeLabelText);
+
+const ageInput = inputs[1];
+ageInput.addEventListener("keyup", unhideText);
