a861bee4f5e6c196cf446d441f806cb7d0db4a27
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-30 10:57:21 +0100

    Add JS to change background color of p with a random value

--- 02/script.js
+++ 02/script.js
@@ -1,5 +1,24 @@
-let importantItems = document.getElementsByClassName("important");
+const importantItems = document.getElementsByClassName("important");
 
 for (const item of importantItems) {
     item.setAttribute("title","This is an important item");
+}
+
+const images = document.getElementsByTagName('img');
+
+for (const image of images) {
+    if (!image.classList.contains("important"))
+    {
+        image.hidden = true;
+    }
+}
+
+const pElements = document.getElementsByTagName("p");
+
+for (const p of pElements) {
+    console.log(p.innerHTML);
+    p.classList.forEach(element => {
+        console.log(element);
+    });
+    if (p.classList.length === 0) p.style.backgroundColor = 'rgb('+ Math.random()*250 +', '+ Math.random()*250 +', ' + Math.random()*250 +')';
 }
