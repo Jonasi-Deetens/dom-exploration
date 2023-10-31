447f9675129e276f070a6a2ccbb88834bdebf006
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-31 12:23:53 +0100

    Change duplicate cards to other games

--- 03_Collection/collection.js
+++ 03_Collection/collection.js
@@ -9,93 +9,93 @@ const collection = [
         link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "7 Days to Die",
+        developer: "The Fun Pimps",
+        releaseDate: "13 Dec, 2013",
+        genre: ["Action","Adventure", "Indie", "RPG"],
+        image: "./images/7days.jpg",
+        rating: "8.8",
+        link: "https://store.steampowered.com/app/251570/7_Days_to_Die/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Among Us",
+        developer: "Innersloth",
+        releaseDate: "16 Nov, 2018",
+        genre: ["Casual"],
+        image: "./images/among-us.jpg",
+        rating: "9.2",
+        link: "https://store.steampowered.com/app/945360/Among_Us/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Bastion",
+        developer: "Supergiant Games",
+        releaseDate: "16 Aug, 2011",
+        genre: ["Action","RPG", "Indie"],
+        image: "./images/bastion.jpg",
+        rating: "9.5",
+        link: "https://store.steampowered.com/app/107100/Bastion/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Brawlhalla",
+        developer: "Blue Mammoth Games",
+        releaseDate: "17 Oct, 2017",
+        genre: ["Action","Indie", "F2P"],
+        image: "./images/brawlhalla.jpg",
+        rating: "8.3",
+        link: "https://store.steampowered.com/app/291550/Brawlhalla/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Counter Strike 2",
+        developer: "Valve",
+        releaseDate: "21 Aug, 2012",
+        genre: ["Action", "F2P"],
+        image: "./images/counterstrike-2.jpg",
+        rating: "8.7",
+        link: "https://store.steampowered.com/app/730/CounterStrike_2/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Deep Rock Galactic",
+        developer: "Ghost Ship Games",
+        releaseDate: "13 May, 2020",
+        genre: ["Action"],
+        image: "./images/deep-rock-galactic.jpg",
+        rating: "9.7",
+        link: "https://store.steampowered.com/app/548430/Deep_Rock_Galactic/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Destiny 2",
+        developer: "Bungie",
+        releaseDate: "1 Oct, 2019",
+        genre: ["Adventure","F2P", "Action"],
+        image: "./images/destiny-2.jpg",
+        rating: "8.1",
+        link: "https://store.steampowered.com/app/1085660/Destiny_2/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
-        rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        name: "Elden Ring",
+        developer: "FromSoftware Inc.",
+        releaseDate: "25 Feb, 2022",
+        genre: ["Action","RPG"],
+        image: "./images/elden-ring.jpg",
+        rating: "9.2",
+        link: "https://store.steampowered.com/app/1245620/ELDEN_RING/"
 
     },
     {
-        name: "Baldur's Gate 3",
-        developer: "Larian Studios",
-        releaseDate: "3 Aug, 2023",
-        genre: ["Adventure","RPG", "Strategy"],
-        image: "./images/bg3.jpg",
+        name: "Risk of Rain 2",
+        developer: "Hopoo Games",
+        releaseDate: "11 Aug, 2020",
+        genre: ["Action","Indie"],
+        image: "./images/risk-of-rain-2.jpg",
         rating: "9.6",
-        link: "https://store.steampowered.com/app/228280/Baldurs_Gate_Enhanced_Edition/"
+        link: "https://store.steampowered.com/app/632360/Risk_of_Rain_2/"
 
     }
 ];
