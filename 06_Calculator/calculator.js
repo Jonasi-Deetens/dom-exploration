9d7f3f537519711f6a2aae247a51ef69cc15936b
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-02 14:43:12 +0100

    Add calculator project and finish it -_06

---
+++ 06_Calculator/calculator.js
@@ -0,0 +1,53 @@
+const buttonsList = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "X", "1", "2", "3", "-", "0", ".", "=", "+"];
+let mathString = "0"
+
+const main = document.querySelector("main");
+const screenElement = document.createElement("section");
+screenElement.classList.add("screen");
+
+const pElement = document.createElement("p");
+pElement.textContent = mathString;
+pElement.classList.add("screen-message");
+screenElement.appendChild(pElement);
+
+
+const buttonsElement = document.createElement("section");
+buttonsElement.classList.add("buttons");
+
+buttonsList.forEach(button => {
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = button;
+    buttonElement.addEventListener("click", addToScreen)
+
+    buttonsElement.appendChild(buttonElement);
+});
+
+main.appendChild(screenElement);
+main.appendChild(buttonsElement);
+
+function addToScreen(event) {
+    const screenText = document.querySelector("screen-message");
+    let key = event.target.textContent;
+    switch (key) {
+        case "X":
+            if (mathString !== "0") mathString += "*";
+            break;
+        case "AC":
+            mathString = "0";
+            break;
+        case "=":
+            evaluateMath();
+            break;
+
+        default:
+            if (mathString === "0" && key !== "/" && key !== "-" && key !== "+" && key !== "%") mathString = event.target.textContent;
+            else if (mathString !== "0") {
+                mathString += event.target.textContent;
+            }
+    }
+    pElement.textContent = mathString;
+}
+
+function evaluateMath() {
+    mathString = Function('return ' + mathString)();
+}
