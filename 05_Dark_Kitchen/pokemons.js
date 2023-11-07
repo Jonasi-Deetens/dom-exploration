6a1df84ee8eeba3e6544a3290031dff2d38917ca
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-07 01:45:09 +0100

    Add shopping cart functionality to add and remove -_05

--- 05_Dark_Kitchen/pokemons.js
+++ 05_Dark_Kitchen/pokemons.js
@@ -2,49 +2,49 @@ const pokemons = [
     {
         name: "Pikachu",
         types: ["electric"],
-        price: "9.50",
+        price: 199.50,
         image: "./images/pikachu.png"
     },
     {
         name: "Clefairy",
         types: ["fairy"],
-        price: "9.50",
+        price: 149.50,
         image: "./images/clefairy.png"
     },
     {
         name: "Helioptile",
         types: ["electric", "normal"],
-        price: "9.50",
+        price: 99.50,
         image: "./images/helioptile.png"
     },
     {
         name: "Jigglypuff",
         types: ["normal", "fairy"],
-        price: "9.50",
+        price: 49.50,
         image: "./images/jigglypuff.png"
     },
     {
         name: "Jolteon",
         types: ["electric"],
-        price: "9.50",
+        price: 199.50,
         image: "./images/jolteon.png"
     },
     {
         name: "Marill",
         types: ["water", "fairy"],
-        price: "9.50",
+        price: 99.50,
         image: "./images/marill.png"
     },
     {
         name: "Rotom",
         types: ["electric", "ghost"],
-        price: "9.50",
+        price: 299.50,
         image: "./images/rotom.png"
     },
     {
         name: "Togepi",
         types: ["fairy"],
-        price: "9.50",
+        price: 49.50,
         image: "./images/togepi.png"
     }
 ];
--- 05_Dark_Kitchen/script.js
+++ 05_Dark_Kitchen/script.js
@@ -1,6 +1,19 @@
 import { pokemons } from "./pokemons.js";
 
-const main = document.querySelector("main");
+let totalPrice = 0;
+let cartIsOpen = false;
+let shoppingCart = [];
+
+const cartButton = document.querySelector(".shopping-basket-button");
+cartButton.addEventListener("click", openCart);
+
+const clearCartButton = document.querySelector(".clear-cart-button");
+clearCartButton.addEventListener("click", clearShoppingCart);
+
+const searchBar = document.querySelector("#search");
+searchBar.addEventListener("keyup", filterCards);
+
+const main = document.querySelector("main").children[1];
 pokemons.forEach(pokemon => {
 
     const card = document.createElement("section");
@@ -60,14 +73,132 @@ pokemons.forEach(pokemon => {
     //Add buy button
     const buyButton = document.createElement("button");
     buyButton.classList.add("buy-button");
-    buyButton.textContent = "Buy";
+    buyButton.textContent = "Catch";
+    buyButton.addEventListener("click", () => { addToCart(pokemon) });
     card.appendChild(buyButton);
     
     //Add price paragraph
     const priceParagraph = document.createElement("p");
-    priceParagraph.textContent = pokemon.price + " $";
+    priceParagraph.textContent = pokemon.price.toFixed(2) + " $";
     priceParagraph.classList.add("card-price");
     card.appendChild(priceParagraph);
 
     main.appendChild(card);
+    updateTotalPrice();
 });
+
+function addToCart(pokemon) {
+    let containsPokemon = false;
+    shoppingCart.forEach(cartItem => {
+        if (cartItem.pokemon === pokemon) {
+            containsPokemon = true;
+            cartItem.amount += 1;
+        }
+    });
+    
+    if (!containsPokemon) {
+        shoppingCart.push({
+            pokemon: pokemon,
+            amount: 1
+        })
+    }
+
+    updateShoppingCart();
+}
+
+function clearShoppingCart() {
+    shoppingCart = [];
+    updateShoppingCart();
+}
+
+function openCart() {
+    const cart = document.querySelector("#shopping-cart-content");
+    if (cartIsOpen) cart.style.display = "none";
+    else cart.style.display = "block";
+
+    cartIsOpen = !cartIsOpen;
+}
+
+function filterCards(event) {
+    const titles = document.querySelectorAll(".card-title");
+    const tags = document.querySelectorAll(".span-item");
+    
+    titles.forEach(title => {
+        if (title.textContent.toLowerCase().includes(event.target.value.toLowerCase())) title.parentElement.style.display = "block";
+        else title.parentElement.style.display = "none";
+    })
+}
+
+function updateShoppingCart() {
+    const cart = document.querySelector("#shopping-cart-content");
+    const tbl = document.querySelector("#shopping-cart-table");
+    tbl.innerHTML = "";
+
+    shoppingCart.forEach(row => {
+        const tr = tbl.insertRow();
+
+        const cellOne = tr.insertCell();
+        cellOne.textContent = row.pokemon.name;
+        cellOne.style.float = "left";
+
+        const cellTwo = tr.insertCell();
+        cellTwo.textContent = row.pokemon.price.toFixed(2) + " $";
+
+        const cellThree = tr.insertCell();
+        cellThree.textContent = "x";
+        const input = document.createElement("input");
+        input.type = "number";
+        input.min = "0";
+        input.value = row.amount;
+        input.name = row.pokemon.name + "Amount";
+        input.classList.add("cart-amount-input");
+        input.addEventListener("change", (e) => { changeAmount(e, row.pokemon) });
+        cellThree.appendChild(input);
+
+        const cellFour = tr.insertCell();
+        cellFour.textContent = "Price: " + (row.pokemon.price * row.amount).toFixed(2) + " $" ;
+        cellFour.style.float = "left";
+
+        const cellFive = tr.insertCell();
+        const removeButton = document.createElement("button");
+        removeButton.appendChild(document.createTextNode("X"));
+        removeButton.classList.add("remove-button");
+        removeButton.addEventListener("click", () => { removeCartItem(row.pokemon) });
+        cellFive.appendChild(removeButton);
+
+    });
+
+    updateTotalPrice();
+    cart.appendChild(tbl);
+}
+
+function changeAmount(event, pokemon) {
+    shoppingCart.forEach(cartItem => {
+        if (cartItem.pokemon === pokemon) {
+            if (event.target.value >= 0) cartItem.amount = event.target.value;
+            else event.target.value = 0;
+        }
+    });
+    updateShoppingCart();
+}
+
+function removeCartItem(pokemon) {
+    shoppingCart.forEach(cartItem => {
+        console.log(cartItem.pokemon);
+        console.log(shoppingCart.indexOf(cartItem));
+        if (cartItem.pokemon === pokemon) shoppingCart.splice(shoppingCart.indexOf(cartItem),1);
+    });
+    updateShoppingCart();
+}
+
+function updateTotalPrice() {
+    const priceElements = document.querySelectorAll(".total-price");
+    totalPrice = 0;
+    shoppingCart.forEach(cartItem => {
+        totalPrice += cartItem.pokemon.price * cartItem.amount;
+    });
+
+    priceElements.forEach(priceElement => {
+        priceElement.textContent = totalPrice.toFixed(2);
+    });
+}
